e86dbfa0a3e974e23ef87b7e7ac6367d24e9f27f
zoro desktop <roronoazorolinux@gmail.com>
2022-07-17 17:57:02 +0300

    some changes

--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,31 @@
 #include<stdio.h>
 
-int main(int argc , char * argv[]){
+int main(int argc, char * argv[]){
+
+int a ,b;
+a = 0;
+b = 1;
 
 
-//this is a comment.
+
+for (int i = 0 ; i < 20 ; i++ ){
+a = b;
+while(a < b+i){
+
+printf("(%d)",a-1);
+a++;
+
+}
+printf("\n");
+b++;
+}
+
+
+
+
+printf("\n");
 printf("hello world");
-printf("\nhello %s",argv[1]);
+printf("\nhello %s\n",argv[1]);
 
 return 0;
 }
