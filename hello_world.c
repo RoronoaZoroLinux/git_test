d2334af542c036802e4c073500ca1e1e746f741d
zoro desktop <roronoazorolinux@gmail.com>
2022-07-17 21:07:07 +0300

    made final changes in hello world.c , and hello world executable.

--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,45 @@
 #include<stdio.h>
 
-int main(int argc , char * argv[]){
+int main(int argc, char * argv[]){
 
 
-//this is a comment.
+printf("\n");
 printf("hello world");
-printf("\nhello %s",argv[1]);
+printf("\nhello %s\n",argv[1]);
+
+int sayi = 0;
+int loop = 1;
+int limit = 0;
+
+limitistoosmal:
+printf("Write a number (recommended 10 - 100 ): ");
+scanf("%d",&limit);
+
+if(limit < 0){
+printf("\nyou can't enter a negative value");
+printf("\n");
+goto limitistoosmal;
+
+}
+
+printf("\n");
+
+for (int i = 0; sayi < limit ; i++){
+
+    while(loop < i){
+    printf("[ %d ]",sayi);
+    sayi++;
+    loop++;
+    }
+    printf("\n");
+    loop = 0;
+}
+
+
+
 
 return 0;
 }
+
+
+
