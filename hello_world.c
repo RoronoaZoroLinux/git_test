040ee9bd9e110467b97e245e87f20a92b05f857a
zoro desktop <roronoazorolinux@gmail.com>
2022-07-17 16:47:29 +0300

    fix hello_world.c and add executable

--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 
-int main(int argc , char* argv){
+int main(int argc , char * argv[]){
 
 
 //this is a comment.
 printf("hello world");
-printf("\nhello %s",argv);
+printf("\nhello %s",argv[1]);
 
 return 0;
 }
